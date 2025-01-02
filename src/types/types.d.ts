2de0e87a4ebcacae9934fab9246af47aca6c5383
Divyansh-Gupta-lab <divyansh181199@gmail.com>
2025-01-02 15:04:48 +0530

    Added campaign interface

---
+++ src/types/types.d.ts
@@ -0,0 +1,9 @@
+import { BigNumber } from "bignumber.js"; // or from 'ethers' if you're using ethers.js
+
+export interface CampaignDetails {
+  0: BigNumber; // or number, depending on the library you're using
+  1: BigNumber;
+  2: BigNumber;
+  3: BigNumber;
+  4: string;
+}
